47af71074692f5ed8b9c608bf62a9efcbc30cf79
Brian Muthui <brian.muthui@andela.com>
2019-09-19 18:18:32 +0300

    Add basic button controls

--- main.js
+++ main.js
@@ -11,14 +11,38 @@ const sprite = {
     east: 2,
     west: 3
   }
+};
+
+const grid = {
+  stepsX: 4,
+  stepsY: 4
 }
 
 const animate = () => {
   if(sprite.currentXPosition === (sprite.stepsX + 1)) {
     sprite.currentXPosition = 0;
   }
-  document.getElementById('robot').style.backgroundPosition = `-${sprite.offsetX * sprite.currentXPosition}px ${sprite.currentYPosition}`;
+  document.getElementById('robot').style.backgroundPosition = `-${sprite.offsetX * sprite.currentXPosition}px -${sprite.offsetY * sprite.currentYPosition}px`;
   sprite.currentXPosition++;
 };
 
 const animeInterval = setInterval(animate, 100);
+
+/**
+ * 
+ * @param {string} direction
+ * @param {number} offset 
+ */
+const move = (direction) => {
+  sprite.currentYPosition = sprite.positions[direction];
+};
+
+document.addEventListener('DOMContentLoaded', () => { 
+  const buttons = document.getElementsByTagName('button');
+  console.log(buttons);
+  [...buttons].forEach(button => {
+    button.onclick = () => {
+      move(button.dataset.controlDirection);
+    }
+  });
+}, false);
