7e30313b96462e89cca0c2640a551c42557533c7
Brian Muthui <brian.muthui@andela.com>
2019-09-19 18:36:16 +0300

    Throw error if wrong direction is provided

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
-// all sprite properties
 const sprite = {
   offsetX: 100,
   offsetY: 100,
   stepsX: 6,
   currentXPosition: 0,
   currentYPosition: 0,
-  positions: {
+  directions: {
     south: 0,
     north: 1,
     east: 2,
@@ -18,6 +17,9 @@ const grid = {
   stepsY: 4
 }
 
+/**
+ * Animates walking movement
+ */
 const animate = () => {
   if(sprite.currentXPosition === (sprite.stepsX + 1)) {
     sprite.currentXPosition = 0;
@@ -25,21 +27,22 @@ const animate = () => {
   document.getElementById('robot').style.backgroundPosition = `-${sprite.offsetX * sprite.currentXPosition}px -${sprite.offsetY * sprite.currentYPosition}px`;
   sprite.currentXPosition++;
 };
-
 const animeInterval = setInterval(animate, 100);
 
 /**
+ * Shifts direction of movement
+ * @param {string} direction One of the directions in sprite directions
  * 
- * @param {string} direction
- * @param {number} offset 
  */
-const move = (direction) => {
-  sprite.currentYPosition = sprite.positions[direction];
+const move = direction => {
+  if(!Object.keys(sprite.directions).includes(direction)) {
+    throw new Error('Invalid control direction configuration');
+  }
+  sprite.currentYPosition = sprite.directions[direction];
 };
 
-document.addEventListener('DOMContentLoaded', () => { 
+document.addEventListener('DOMContentLoaded', () => {
   const buttons = document.getElementsByTagName('button');
-  console.log(buttons);
   [...buttons].forEach(button => {
     button.onclick = () => {
       move(button.dataset.controlDirection);
