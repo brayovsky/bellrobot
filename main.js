094d2d2eb1c3181233fb77c9d75bc17da37c3b29
Brian Muthui <brian.muthui@andela.com>
2019-09-19 21:01:42 +0300

    Moove character using controls available

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+const characterId = 'robot', moveTime = 1000;
 const sprite = {
   offsetX: 100,
   offsetY: 100,
@@ -13,8 +14,12 @@ const sprite = {
 };
 
 const grid = {
-  stepsX: 4,
-  stepsY: 4
+  size: 500,
+  unit: 100,
+  characterPosition: {
+    x: 0,
+    y: 0
+  }
 }
 
 /**
@@ -24,10 +29,9 @@ const animate = () => {
   if(sprite.currentXPosition === (sprite.stepsX + 1)) {
     sprite.currentXPosition = 0;
   }
-  document.getElementById('robot').style.backgroundPosition = `-${sprite.offsetX * sprite.currentXPosition}px -${sprite.offsetY * sprite.currentYPosition}px`;
+  document.getElementById(characterId).style.backgroundPosition = `-${sprite.offsetX * sprite.currentXPosition}px -${sprite.offsetY * sprite.currentYPosition}px`;
   sprite.currentXPosition++;
 };
-const animeInterval = setInterval(animate, 100);
 
 /**
  * Shifts direction of movement
@@ -39,6 +43,36 @@ const move = direction => {
     throw new Error('Invalid control direction configuration');
   }
   sprite.currentYPosition = sprite.directions[direction];
+  const directionToStyleMap = {
+    north: {
+      increment: -1,
+      style: 'top',
+      axis: 'y'
+    },
+    south: {
+      increment: 1,
+      style: 'top',
+      axis: 'y'
+    },
+    east: {
+      increment: 1,
+      style: 'left',
+      axis: 'x'
+    },
+    west: {
+      increment: -1,
+      style: 'left',
+      axis: 'x'
+    }
+  }
+
+  const newOffsetPosition = grid.characterPosition[directionToStyleMap[direction].axis] + directionToStyleMap[direction].increment * 100;
+  if(newOffsetPosition >= 0 && newOffsetPosition <= grid.size - grid.unit) {
+    const animeInterval = setInterval(animate, 100);
+    document.getElementById(characterId).style[directionToStyleMap[direction].style] = `${newOffsetPosition}px`;
+    grid.characterPosition[directionToStyleMap[direction].axis] = newOffsetPosition;
+    setTimeout(() => clearInterval(animeInterval), moveTime);
+  }
 };
 
 document.addEventListener('DOMContentLoaded', () => {
