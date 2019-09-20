6219f1f9bb68ba58a910fa1a260b57c3ce972531
Brian Muthui <brian.muthui@andela.com>
2019-09-20 14:48:37 +0300

    Add movement tests

--- tests/index.test.js
+++ tests/index.test.js
@@ -2,6 +2,27 @@ import IndexPage from './pages/indexPage';
 
 fixture `Index` .page('../index.html')
 
-test('Robot goes up', async t => {
-  await IndexPage.goUp();
+test('Movement along y-axis', async t => {
+  await IndexPage.goDown(1);
+  await t.expect(IndexPage.robot.getStyleProperty('top')).eql('100px');
+  await IndexPage.goUp(1);
+  await t.expect(IndexPage.robot.getStyleProperty('top')).eql('0px');
+});
+
+test('Movement along x-axis', async t => {
+  await IndexPage.goRight(1);
+  await t.expect(IndexPage.robot.getStyleProperty('left')).eql('100px');
+  await IndexPage.goLeft(1);
+  await t.expect(IndexPage.robot.getStyleProperty('left')).eql('0px');
+});
+
+test('Robot not move outside grid', async t => {
+  await IndexPage.goLeft(1);
+  await t.expect(IndexPage.robot.getStyleProperty('left')).eql('0px');
+  await IndexPage.goUp(1);
+  await t.expect(IndexPage.robot.getStyleProperty('top')).eql('0px');
+  await IndexPage.goRight(10);
+  await t.expect(IndexPage.robot.getStyleProperty('left')).eql('400px');
+  await IndexPage.goDown(10);
+  await t.expect(IndexPage.robot.getStyleProperty('top')).eql('400px');  
 });
--- tests/pages/indexPage.js
+++ tests/pages/indexPage.js
@@ -4,9 +4,45 @@ class IndexPage {
   get upButton() {
     return Selector("button[data-control-direction='north']");
   }
+  get downButton() {
+    return Selector("button[data-control-direction='south']");
+  }
+  get rightButton() {
+    return Selector("button[data-control-direction='east']");
+  }
+  get leftButton() {
+    return Selector("button[data-control-direction='west']");
+  }
 
-  async goUp() {
-    await t.click(this.upButton)
+  get robot() {
+    return Selector("#robot");
+  }
+
+  async goUp(steps) {
+    await this._clickButton(this.upButton, steps);
+  }
+
+  async goDown(steps) {
+    await this._clickButton(this.downButton, steps);
+  }
+
+  async goRight(steps) {
+    await this._clickButton(this.rightButton, steps);
+  }
+
+  async goLeft(steps) {
+    await this._clickButton(this.leftButton, steps);
+  }
+
+  async _clickButton(button, times) {
+    const clicks = [];
+    while(times) {
+      clicks.push(new Promise((resolve, reject) => {
+        resolve(t.click(button));
+      }));
+      times--;
+    }
+    await Promise.all(clicks);
   }
 }
 
